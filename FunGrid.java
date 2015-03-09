f441944859427aba2b3a17dd57edc789d984fb9b
codechai <shubhi3296@gmail.com>
2015-03-09 19:07:36 +0530

    changing

--- FunGrid.java
+++ FunGrid.java
@@ -1,14 +1,15 @@
 
 package fungrid;
 public class FunGrid {
-    
+
     public FunGrid(){
         new ButtonGrid(10,10);
-        
+
     }
 
     public static void main(String[] args) {
         new FunGrid();
+        System.out.println("hello there");
     }
-    
+
 }
