6cb852515f5160f25ef079f76f9fca7197e782bb
codechai <shubhi3296@gmail.com>
2015-03-09 13:36:36 +0530

    committed

---
+++ FunGrid.java
@@ -0,0 +1,14 @@
+
+package fungrid;
+public class FunGrid {
+    
+    public FunGrid(){
+        new ButtonGrid(10,10);
+        
+    }
+
+    public static void main(String[] args) {
+        new FunGrid();
+    }
+    
+}
